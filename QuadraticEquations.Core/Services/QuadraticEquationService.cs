1702698e1f626ebd7021611b46d59f5c8c6cbd00
AndresRios <andresriosbolivar27@gmail.com>
2019-11-23 07:44:52 -0500

    QuadraticEquation

--- QuadraticEquations.Core/Services/QuadraticEquationService.cs
+++ QuadraticEquations.Core/Services/QuadraticEquationService.cs
@@ -9,8 +9,8 @@ namespace QuadraticEquations.Core.Services
         
         public void GetQuadraticEquation(double a, double b, double c, out double x1, out double x2)
         {
-            x1 = (-b + Math.Sqrt(b * b - 4 * a * c)) / (2 * a);
-            x2 = (-b - Math.Sqrt(b * b - 4 * a * c)) / (2 * a);
+            x1 = (-b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a);
+            x2 = (-b - Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a);
         }
     }
 }
